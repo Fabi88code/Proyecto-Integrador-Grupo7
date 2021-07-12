640c5d36c4449dc36fc4c8fbe4bb644da5111ae8
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-07-12 12:03:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -31,6 +31,25 @@ app.listen(3000, () => {
 });
 */
 
-app.listen(process.env.PORT || 3000, function() {
+/*app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
+});*/
+
+const PORT = process.env.PORT || 3000;
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(PORT, () => { 
+   console.log ("Servidor corriendo en puerto "+PORT);
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req,res) =>{   
+    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
+   res.sendFile(path.resolve(__dirname, './views/index.html'));   // Permite enviar un archivo HTML
+
 });
