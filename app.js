b071406a67c9f66760d7e16625929c4159bda599
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-07-12 10:47:00 -0300

    heroku

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const path = require('path');
 
+const app = express();
+
 app.get('/', (req,res) =>{
-  res.sendFile(path.join(__dirname, './views/index.html'));
+   res.sendFile(path.join(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
 });
+
+app.get('/registro', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));
+  });
+
+  app.get('/registro', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/carrito.html'));
+  });
+
+  app.get('/registro', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/producto.html'));
+  });
+
+  app.get('/registro', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/registro.html'));
+  });
 
 app.use(express.static(path.join(__dirname, './public')));
-
-
-app.listen(3000, function() {
-  console.log("Corriendo servidor en el puerto 3000");
+app.use(express.static(path.join(__dirname, './views')));
+/*
+app.listen(3000, () => {
+  console.log("Servidor corriendo");
 });
+*/
 
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+});
